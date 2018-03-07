0929f261f25d039a888ff1464c396c4c4c676ac2
onokumus <onokumus@gmail.com>
2018-03-07 21:10:44 +0300

    changed: temp folder name

--- assemblefile.js
+++ assemblefile.js
@@ -11,9 +11,9 @@ app.task('init', function(cb) {
     app.data('github', app.options.github);
     app.helper('markdown', markdown);
     app.data('pkg', pkg);
-    app.layouts(path.join(__dirname, './templates/lay/*.hbs'));
-    app.partials(path.join(__dirname, './templates/partials/**/*.hbs'));
-    app.pages(path.join(__dirname, './templates/pages/*.hbs'));
+    app.layouts(path.join(__dirname, './tmp/lay/*.hbs'));
+    app.partials(path.join(__dirname, './tmp/partials/**/*.hbs'));
+    app.pages(path.join(__dirname, './tmp/pages/*.hbs'));
     app.option('layout', 'default');
     cb();
 });
