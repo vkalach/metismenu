f82b6e88514c5696fdc3cc1df37af93a5983f327
onokumus <onokumus@gmail.com>
2018-05-09 02:09:44 +0300

    :penguin: remove navbar height

--- assets/js/mm-event2.js
+++ assets/js/mm-event2.js
@@ -1,10 +1,9 @@
 $(function() {
-  var navbarHeight = $('.navbar').height();
   $('#menu')
     .metisMenu()
     .on('shown.metisMenu', function(event) {
       $('body,html').animate({
-        scrollTop: $(event.target).parent('li').position().top - navbarHeight
+        scrollTop: $(event.target).parent('li').offset().top
       }, 600);
     });
 });
